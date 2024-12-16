987b5853c98b6f710ad96b125f5dac41b590411e
Vic102 <victorriddlestoneaguilar@gmail.com>
2024-12-16 14:21:32 +0100

    Añadidos los filtros

--- src/components/sitiosData.js
+++ src/components/sitiosData.js
@@ -3,180 +3,210 @@ export const sitios = [
         id: 1,
         nombre: "Palacio_Real_de_Madrid",
         imagen: "/images/monumentos/palacio_real.webp",
-        localizacion: { latitud: 40.4170, longitud: -3.7147 }
+        localizacion: { latitud: 40.4170, longitud: -3.7147 },
+        filtro: ["Palacios"]
     },
     {
         id: 2,
         nombre: "Puerta_de_Alcalá",
         imagen: "/images/monumentos/puerta_alcala.webp",
-        localizacion: { latitud: 40.4195, longitud: -3.6921 }
+        localizacion: { latitud: 40.4195, longitud: -3.6921 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 3,
         nombre: "Templo_de_Debod",
         imagen: "/images/monumentos/templo_debod.webp",
-        localizacion: { latitud: 40.4256, longitud: -3.7160 }
+        localizacion: { latitud: 40.4256, longitud: -3.7160 },
+        filtro: ["Monumentos", "Religión", "Al aire libre"]
     },
     {
         id: 4,
         nombre: "Catedral_de_la_Almudena",
         imagen: "/images/monumentos/catedral_almudena.webp",
-        localizacion: { latitud: 40.4154, longitud: -3.7142 }
+        localizacion: { latitud: 40.4154, longitud: -3.7142 },
+        filtro: ["Religión"]
     },
     {
         id: 5,
         nombre: "Plaza_Mayor_de_Madrid",
         imagen: "/images/monumentos/plaza_mayor.webp",
-        localizacion: { latitud: 40.4154, longitud: -3.7074 }
+        localizacion: { latitud: 40.4154, longitud: -3.7074 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 6,
         nombre: "Fuente_de_Cibeles",
         imagen: "/images/monumentos/fuente_cibeles.webp",
-        localizacion: { latitud: 40.4196, longitud: -3.6919 }
+        localizacion: { latitud: 40.4196, longitud: -3.6919 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 7,
         nombre: "Museo_del_Prado",
         imagen: "/images/monumentos/museo_prado.webp",
-        localizacion: { latitud: 40.4138, longitud: -3.6921 }
+        localizacion: { latitud: 40.4138, longitud: -3.6921 },
+        filtro: ["Museos"]
     },
     {
         id: 8,
         nombre: "Puerta_del_Sol",
         imagen: "/images/monumentos/puerta_sol.webp",
-        localizacion: { latitud: 40.4168, longitud: -3.7038 }
+        localizacion: { latitud: 40.4168, longitud: -3.7038 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 9,
         nombre: "Gran_Vía",
         imagen: "/images/monumentos/gran_via.webp",
-        localizacion: { latitud: 40.4190, longitud: -3.7057 }
+        localizacion: { latitud: 40.4190, longitud: -3.7057 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 10,
         nombre: "Teatro_Real_(Madrid)",
         imagen: "/images/monumentos/teatro_real.webp",
-        localizacion: { latitud: 40.4184, longitud: -3.7113 }
+        localizacion: { latitud: 40.4184, longitud: -3.7113 },
+        filtro: ["Monumentos"]
     },
     {
         id: 11,
         nombre: "Parque_del_Retiro_de_Madrid",
         imagen: "/images/monumentos/parque_retiro.webp",
-        localizacion: { latitud: 40.4138, longitud: -3.6841 }
+        localizacion: { latitud: 40.4138, longitud: -3.6841 },
+        filtro: ["Al aire libre", "Naturaleza"]
     },
     {
         id: 12,
         nombre: "Plaza_de_España_(Madrid)",
         imagen: "/images/monumentos/plaza_espana.webp",
-        localizacion: { latitud: 40.4243, longitud: -3.7135 }
+        localizacion: { latitud: 40.4243, longitud: -3.7135 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 13,
         nombre: "Real_basílica_de_San_Francisco_el_Grande",
         imagen: "/images/monumentos/san_francisco.webp",
-        localizacion: { latitud: 40.4110, longitud: -3.7137 }
+        localizacion: { latitud: 40.4110, longitud: -3.7137 },
+        filtro: ["Religión"]
     },
     {
         id: 14,
         nombre: "El_Rastro_de_Madrid",
         imagen: "/images/monumentos/rastro.webp",
-        localizacion: { latitud: 40.4119, longitud: -3.7114 }
+        localizacion: { latitud: 40.4119, longitud: -3.7114 },
+        filtro: ["Al aire libre"]
     },
     {
         id: 15,
         nombre: "Estadio_Santiago_Bernabéu",
         imagen: "/images/monumentos/Bernabeu.webp",
-        localizacion: { latitud: 40.4531, longitud: -3.6883 }
+        localizacion: { latitud: 40.4531, longitud: -3.6883 },
+        filtro: ["Monumentos"]
     },
     {
         id: 16,
         nombre: "Palacio_de_Cristal_del_Retiro",
         imagen: "/images/monumentos/palacio_cristal.webp",
-        localizacion: { latitud: 40.4112, longitud: -3.6901 }
+        localizacion: { latitud: 40.4112, longitud: -3.6901 },
+        filtro: ["Palacios"]
     },
     {
         id: 17,
         nombre: "Museo_Nacional_Centro_de_Arte_Reina_Sofía",
         imagen: "/images/monumentos/reina_sofia.webp",
-        localizacion: { latitud: 40.4075, longitud: -3.6922 }
+        localizacion: { latitud: 40.4075, longitud: -3.6922 },
+        filtro: ["Museos"]
     },
     {
         id: 18,
         nombre: "Casa_de_Campo",
         imagen: "/images/monumentos/casa_campo.webp",
-        localizacion: { latitud: 40.4026, longitud: -3.7380 }
+        localizacion: { latitud: 40.4026, longitud: -3.7380 },
+        filtro: ["Naturaleza"]
     },
     {
         id: 19,
         nombre: "Círculo_de_Bellas_Artes",
         imagen: "/images/monumentos/circulo_bellas_artes.webp",
-        localizacion: { latitud: 40.4193, longitud: -3.7072 }
+        localizacion: { latitud: 40.4193, longitud: -3.7072 },
+        filtro: ["Museos"]
     },
     {
         id: 20,
         nombre: "Museo_Thyssen-Bornemisza",
         imagen: "/images/monumentos/thyssen.webp",
-        localizacion: { latitud: 40.4138, longitud: -3.6948 }
+        localizacion: { latitud: 40.4138, longitud: -3.6948 },
+        filtro: ["Museos"]
     },
     {
         id: 21,
         nombre: "Torre_Eiffel",
         imagen: "/images/monumentos/eiffel_tower.webp",
-        localizacion: { latitud: 48.8584, longitud: 2.2945 }
+        localizacion: { latitud: 48.8584, longitud: 2.2945 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 22,
         nombre: "Taj_Mahal",
         imagen: "/images/monumentos/taj_mahal.webp",
-        localizacion: { latitud: 27.1751, longitud: 78.0421 }
+        localizacion: { latitud: 27.1751, longitud: 78.0421 },
+        filtro: ["Monumentos", "Religión"]
     },
     {
         id: 23,
         nombre: "Coliseo",
         imagen: "/images/monumentos/coliseo_romano.webp",
-        localizacion: { latitud: 41.8902, longitud: 12.4922 }
+        localizacion: { latitud: 41.8902, longitud: 12.4922 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 24,
         nombre: "Gran_Muralla_China",
         imagen: "/images/monumentos/gran_muralla_china.webp",
-        localizacion: { latitud: 40.4319, longitud: 116.5704 }
+        localizacion: { latitud: 40.4319, longitud: 116.5704 },
+        filtro: ["Monumentos", "Al aire libre"]
     },
     {
         id: 25,
         nombre: "Cristo_Redentor",
         imagen: "/images/monumentos/cristo_redentor.webp",
-        localizacion: { latitud: -22.9519, longitud: -43.2105 }
+        localizacion: { latitud: -22.9519, longitud: -43.2105 },
+        filtro: ["Religión"]
     },
     {
         id: 26,
         nombre: "Machu_Picchu",
         imagen: "/images/monumentos/machu_picchu.webp",
-        localizacion: { latitud: -13.1631, longitud: -72.5450 }
+        localizacion: { latitud: -13.1631, longitud: -72.5450 },
+        filtro: ["Naturaleza"]
     },
     {
         id: 27,
         nombre: "Estatua_de_la_Libertad",
         imagen: "/images/monumentos/estatua_de_libertad.webp",
-        localizacion: { latitud: 40.6892, longitud: -74.0445 }
+        localizacion: { latitud: 40.6892, longitud: -74.0445 },
+        filtro: ["Monumentos"]
     },
     {
         id: 28,
         nombre: "Petra",
         imagen: "/images/monumentos/petra.webp",
-        localizacion: { latitud: 30.3285, longitud: 35.4444 }
+        localizacion: { latitud: 30.3285, longitud: 35.4444 },
+        filtro: ["Monumentos", "Religión"]
     },
     {
         id: 29,
         nombre: "Sagrada_Familia",
         imagen: "/images/monumentos/sagrada_familia.webp",
-        localizacion: { latitud: 41.4036, longitud: 2.1744 }
+        localizacion: { latitud: 41.4036, longitud: 2.1744 },
+        filtro: ["Religión", "Monumentos"]
     },
     {
         id: 30,
         nombre: "Gran_Pirámide_de_Guiza",
         imagen: "/images/monumentos/pyramids_of_giza.webp",
-        localizacion: { latitud: 29.9792, longitud: 31.1342 }
+        localizacion: { latitud: 29.9792, longitud: 31.1342 },
+        filtro: ["Monumentos", "Religión"]
     }
 ];
